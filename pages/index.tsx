9bc0a5af250c2b8dbbbd5f1ba94c88c6ce3f207a
Çağrı <cagri058@hotmail.com>
2023-03-05 00:24:54 +0300

    .

---
+++ pages/index.tsx
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import { Box, Center, Spacer, Stack } from "@chakra-ui/react"
+import NavBar from "../components/NavBar"
+import Disconnected from '../components/Disconnected'
+import { useWallet } from "@solana/wallet-adapter-react"
+import Connected from "../components/Connected"
+import type { NextPage } from "next"
+const inter = Inter({ subsets: ['latin'] })
+
+const Home: NextPage = () => {
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Buildoors</title>
+        <meta name="The NFT Collection for Buildoors" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Box
+        w="full"
+        h="calc(100vh)"
+        bgImage={"url(/home-background.svg)"}
+        backgroundPosition="center"
+      >
+        <Stack w="full" h="calc(100vh)" justify="center">
+					{ /* NavBar */ }
+          <NavBar />
+          <Spacer />
+          <Center>{Connected ? <Connected /> : <Disconnected />}</Center>
+          <Spacer />
+
+          <Center>
+            <Box marginBottom={4} color="white">
+              <a
+                href="https://twitter.com/_buildspace"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                built with @_buildspace
+              </a>
+            </Box>
+          </Center>
+        </Stack>
+      </Box>
+    </div>
+  )
+}
+
+export default Home
